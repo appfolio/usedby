b84a797eb99cb3609ae341d63dc66d34d5cdb012
Magne Land <magne.land@appfolio.com>
2020-03-05 18:26:08 -0800

    Fix GemfileNotFound error

--- lib/usedby.rb
+++ lib/usedby.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
+# set environment variable to avoid GemfileNotFound error
+ENV['BUNDLE_GEMFILE'] = File.join(__dir__, 'usedby', 'gems.rb') # new name for a Gemfile
 require 'usedby/cli'
 require 'usedby/version'
---
+++ lib/usedby/gems.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+# This is a dummy Gemfile used by ENV['BUNDLE_GEMFILE']
+# There are no actual gems here
